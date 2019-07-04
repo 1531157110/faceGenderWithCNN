16a6e771f9b8b93863e7f269cd0e2a845c1da4ea
1531157110 <52371867+1531157110@users.noreply.github.com>
2019-07-04 17:45:24 +0800

    第三次修改

--- traingender.py
+++ traingender.py
@@ -1,14 +1,17 @@
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import  Dense, Dropout, Flatten
 from keras.optimizers import SGD
+from keras.callbacks import TensorBoard
+from keras.callbacks import ModelCheckpoint
 from read_data import getPicLab
 import numpy as np
 import keras
 import tensorflow as tf
 # from tensorflow import keras
 from keras.optimizers import SGD
-
-
+tensorboard = TensorBoard(log_dir='log')
+checkpoint = ModelCheckpoint(filepath='file_model.h5',monitor='val_loss',mode='auto' ,save_best_only='True')
+callback_lists=[tensorboard,checkpoint]
 IMAGE_SIZE = 32
 raw_images, raw_labels = getPicLab()
 # print(raw_labels[1])
@@ -18,53 +21,58 @@ train_input, valid_input, train_output, valid_output =train_test_split(raw_image
 train_input /= 255.0
 valid_input /= 255.0
 
-my_callbacks = [tf.keras.callbacks.EarlyStopping(patience=6, min_delta=0.00001, monitor='val_loss')]
+# my_callbacks = [tf.keras.callbacks.EarlyStopping(patience=6, min_delta=0.00001, monitor='val_loss')]
 
 # face_recognition_model = keras.Sequential()
+
 IMAGE_W=IMAGE_SIZE
 IMAGE_H=IMAGE_SIZE
 model = keras.Sequential([
-    keras.layers.Conv2D(32, (3, 3), input_shape=(IMAGE_W, IMAGE_H, 3), strides=(1, 1), activation='relu'),
+    keras.layers.Conv2D(32, (3, 3), input_shape=(IMAGE_W, IMAGE_H, 3), strides=(1, 1), activation='relu', padding='SAME'),
     keras.layers.MaxPool2D(pool_size=(2, 2)),
-    keras.layers.Conv2D(64, (3, 3), strides=(1, 1), activation='relu'),
+    keras.layers.Conv2D(64, (3, 3), strides=(1, 1), activation='relu', padding='SAME'),
+    # keras.layers.Conv2D(16, (3, 3), strides=(1, 1), activation='relu', padding='SAME'),
     keras.layers.MaxPool2D(pool_size=(2, 2)),
     keras.layers.Flatten(),
-    keras.layers.Dense(96, activation=tf.nn.relu),
+    keras.layers.Dense(512, activation=tf.nn.relu),
     keras.layers.Dropout(0.2),
     keras.layers.Dense(2, activation=tf.nn.softmax)
 ])
 
 model.summary()
-
+epochs=20
 learning_rate = 0.01
 
-decay = 1e-6
+# decay = 1e-6
 
 momentum = 0.8
 
 nesterov = True
 
-sgd_optimizer = SGD(lr = learning_rate, decay = decay,
+sgd_optimizer = SGD(lr=learning_rate, decay=learning_rate/(epochs), momentum=0.9, nesterov=True)
 
-                    momentum = momentum, nesterov = nesterov)
+# sgd_optimizer = SGD(lr = learning_rate, decay = decay,
+#
+#                     momentum = momentum, nesterov = nesterov)
+
+
 
 model.compile(loss = 'categorical_crossentropy',
-
                                optimizer = sgd_optimizer,
-
                                metrics = ['accuracy'])
 model.fit(x=train_input,
           y=train_output,
-          batch_size=24,
-          epochs=10,
+          batch_size=32,
+          epochs=epochs,
           verbose=2,
           shuffle=True,
+          callbacks=callback_lists,
           validation_data = (valid_input, valid_output)
           )
 
-MODEL_PATH = 'face_model.h5'
-model.save(MODEL_PATH)
-print("保存成功")
+# MODEL_PATH = 'face_model.h5'
+# model.save(MODEL_PATH)
+# print("保存成功")
 
 
 
